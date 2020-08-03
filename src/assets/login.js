28013b3462d861e996607315f1a816e3d8d9f732
HRITHIK KRISHNA <hrk999666333@gmail.com>
2020-08-03 11:43:07 +0530

    new update & remove node modules

---
+++ src/assets/login.js
@@ -0,0 +1,20 @@
+let email = document.getElementById("email");
+let pwd = document.getElementById("pwd");
+
+function validate() {
+    if (email.value.trim =="" ||pwd.value.trim =="") 
+    {
+        alert("feild is empty");
+        return false;
+    } else if(pwd.value.length<=5){
+        alert("password strong poor");
+    }else if(pwd.value.length<=8){
+        alert("password strong medium");
+    }else if(pwd.value.length<=10){
+        alert("password is strong ")
+    }
+    else {
+        return true;
+    }
+}
+
